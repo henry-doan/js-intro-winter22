6ff1cf8509dd4d1494a86a81c30f33179a7cec3b
Henry Doan <henrydoan96@gmail.com>
2022-11-16 18:54:06 -0700

    first js notes

---
+++ notes.js
@@ -0,0 +1,60 @@
+// comment
+// command ? 
+
+/*
+* multi line comment
+*
+*/
+
+// Datatype 
+// String 
+// "" ''
+// Integer
+// 12 -123 0
+// Float
+// 1.123 -123.123 0.0
+// Boolean
+// true false
+// Arrays
+// ['bob', 12, true]
+// Hashes
+// { key: value }
+// { first: 'bob'}
+// NAN
+// Undefined
+// // Nil -> null
+
+// + - * / PEMAS ()
+// % < > <= >= == !== === !=
+// && ||
+
+// console.log(2 ** 2)
+
+// console.log(3/2) // will give you a float
+// string Concatenation
+// console.log("Hello " + "World")
+
+// String interpolation
+// let name = 'bob'
+// console.log(`Hello ${name}`)
+
+// ++
+// --
+// += -=
+
+// Variables
+// keyword namevar = obj
+// Name is going camelCase 
+// // can't name it any keyword class type, string 
+// var - old way, we don't use any more, hoisting, global file wide
+// let - new way, reassign
+// const - new way, constant, read only
+// scope, where we are defining them
+
+// var num = 5
+// let num = 5
+//   num = 5
+// const num2 = 5
+// num2 = 5
+
+// const - var, modules / components, functions, 
